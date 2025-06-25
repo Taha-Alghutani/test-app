451d2c0a49bf4534878c09f001c7d0147db8fa95
Taha-BG <alghutanitaha@gmail.com>
2025-06-25 15:20:29 +0300

    ....

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='items/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 import socket
+from .models import Item
 
 # Create your views here.
 def index(request):
     hostname = socket.gethostname()
     return render(request, 'app/index.html', {
         'container_id': hostname
+    })
+def items(request):
+    items = Item.objects.all()
+    return render(request, 'app/item_list.html', {
+        'items': items
     })
