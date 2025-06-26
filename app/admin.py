85f3d2744964db87c310c7a4b9177b08bb849048
Taha-BG <alghutanitaha@gmail.com>
2025-06-27 02:32:40 +0300

    .............

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from app import views
+from .models import Item
+admin.site.register(Item)
 
 
 # Register your models here.
